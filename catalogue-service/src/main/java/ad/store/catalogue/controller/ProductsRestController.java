ba4258cfce6ba43721d05306aa7ab74b76babdcc
Adamets Aleksei <verwylf@yandex.ru>
2025-07-04 18:38:49 +0300

    Added KeyCloak

--- catalogue-service/src/main/java/ad/store/catalogue/controller/ProductsRestController.java
+++ catalogue-service/src/main/java/ad/store/catalogue/controller/ProductsRestController.java
@@ -5,12 +5,15 @@ import ad.store.catalogue.entity.Product;
 import ad.store.catalogue.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.security.Principal;
 import java.util.Map;
 
 @RestController
@@ -21,7 +24,8 @@ public class ProductsRestController {
     private final ProductService productService;
 
     @GetMapping
-    public Iterable<Product> findProducts(@RequestParam(name = "filter", required = false) String filter) {
+    public Iterable<Product> findProducts(@RequestParam(name = "filter", required = false) String filter,
+                                          Principal principal) {
         return this.productService.findAllProducts(filter);
     }
 
--- manager-app/src/main/java/ad/store/manager/controller/ProductsController.java
+++ manager-app/src/main/java/ad/store/manager/controller/ProductsController.java
@@ -5,12 +5,15 @@ import ad.store.manager.client.ProductsRestClient;
 import ad.store.manager.controller.payload.NewProductPayload;
 import ad.store.manager.entity.Product;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
